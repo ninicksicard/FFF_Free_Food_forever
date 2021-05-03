aab98e412f240689db4a2fbab38b3298b3de5bff
Nicolas Sicard <31396919+ninicksicard@users.noreply.github.com>
2021-05-03 18:08:01 -0400

    Update MicrocontrolerMain.ino

--- Software/MicrocontrolerMain/MicrocontrolerMain.ino
+++ Software/MicrocontrolerMain/MicrocontrolerMain.ino
@@ -13,7 +13,7 @@ int preReadDelay = 20; //ms before reading analog input to ensure stabilised sen
 
 
 /////////////////////// general purpos sensor read //////////////////////////////
-void sensorRead(int sensor){
+float sensorRead(int sensor){
   //sensor is a list of 2 ints, [power pin, read pin]
   digitalWrite(sensor[0], HIGH);
   delay(preReadDelay);
@@ -71,7 +71,7 @@ int lightCalFact = 1;
 int lightCalOffset = 0 ;
 int Light_Sensor = [x,y]; // x:photoresistor powerpin y:photoresistor analog input pin number(on ads1115)
 
-void lightSens(){
+float lightSens(){
   float light_level;
   light_level = sensorRead(Light_Sensor) * LightCalFact + LightCalOffset
   return light_level
@@ -98,9 +98,6 @@ void lightSwitch_off(){
  *  air compressor/fan
  *  pressure log and alarms (to notify for leaks)
  */
-
-
-
 
 // -------------------------------------- Aeration ----------------------------------------------
 /* control of the pump for bubbler stone, spray or other.
@@ -134,7 +131,7 @@ int mainLevelCalFact = 1;
 int mainLevelCalOffset = 0;
 int main_Level_Sensor = [x,y]; // x: sensor power pin, y:sensor analog in pin
 
-void mainTankLevel(){
+float mainTankLevel(){
   float Water_Level;
   Water_Level = sensorRead(Main_Tank_Level_Sensor) * mainLevelCalFact + mainLevelCalOffset;
   return Water_Level;
@@ -147,7 +144,7 @@ int secondaryLevelCalFact = 1;
 int secondaryLevelCalOffset = 0;
 int secondary_Level_Sensor = [x,y]; // x: sensor power pin, y:sensor analog in pin
 
-void secondaryTankLevel(){
+float secondaryTankLevel(){
   float Water_Level;
   Water_Level = sensorRead(secondary_Tank_Level_Sensor) * secondaryLevelCalFact + secondaryLevelCalOffset;
   return Water_Level;
@@ -158,16 +155,40 @@ void secondaryTankLevel(){
 // ---------------------------------------- population management ---------------------------------
 /*  photodensity sensor
  */
+////////////////////////////////////////////////////////
+int densityPhotoresistor = [x,y]; // x:photoresistor power pin y:photoresistor analog input pin
 
+float populationDensity(){
+  float density;
+  density = sensorRead(densityPhotoresistor);
+  return density
+}
 
 
 // --------------------------------------------- Extraction ----------------------------------------
 /*  filter convayor motor control
- *  vaccuum switch
+ *  vacuum switch
  *  extraction pump/valve
  */
 
+int extractionPump = x; //x:extraction pump relay switch
+int vacuumPump = x; // x:vaccume pump relay number 
 
+void extraction_on(){ // extraction time is in seconds
+  switchRelay(extractionPump,HIGH);
+}
+
+void extraction_off(){
+  switchRelay(extractionPump,LOW);
+}
+
+void vaccumePump_on(){
+  switchRelay(vacuumPump,HIGH);
+}
+
+void vacuumPump_off(){
+  switchRelay(vacuumPump,LOW);
+}
 
 // ------------------------------------------Nutrient management ------------------------------------
 /*  solid fertiliser dosing actuator(s)(archimedes screw motor)
@@ -175,16 +196,34 @@ void secondaryTankLevel(){
  *  magnetic stiring motor
  *  nutrient output pump/valve
  */
+//right now, considering a premixed fluid that is added as the algea is extracted
 
+int peristalticPump = x //x: peristaltic pump power pins
+int stirerMotor = x //x: stiring motor power pin relay
 
+void nutrienPump_on(){
+  switchRelay(peristalticPump,HIGH);
+}
+
+void nutrientPump_off(){
+  switchRelay(peristalticPump,LOW);
+}
 
 // -------------------------------------------- Ph management -------------------------------------- 
 /*  ph sensor
  *  ph dosing(screw motor)
  */
 
+int phSensor = [x,y] //x: main power relay pin or number y: ph analog input  
+int phCalFact=32;
+int phCalOffset1=0.82;
+int phCalOffset2=7;
 
-
+float ph_sens(){
+  float ph;
+  ph = (phCalOffset1-sensorRead(phSensor))*phCalFact+phCalOffset2;
+  return ph;
+}
 
 // ----------------------------------------  Temperature management --------------------------------
 /*  thermometers input
@@ -199,7 +238,7 @@ int tempCalFact = 1;
 int tempCalOffset = 0 ;
 int thermometer = [x,y]; // x:thermometer power pin, y:thermometer analog input pin
 
-void temperature_sens(int sensor){
+float temperature_sens(int sensor){
   float temp;
   temp = sensorRead(thermometer) * tempCalFact + tempCalOffset;
   return temp;
